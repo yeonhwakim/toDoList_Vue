bce46100a9b32ac847e57e3cfb5c460d74ae7b0d
yeonhwa <yeonhwa6717@gmail.com>
2020-01-12 00:08:03 +0900

    Set App component. - ```App``` 컴포넌트에서 데이터를 한번에 처리 - ```todoItems``` 데이터와 addTodo 메서드 추가 - ```todoInput```에 데이터 전달 - ```todoList```에 addTodo 전달

--- toDoList/src/App.vue
+++ toDoList/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata=" todoItems"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -14,6 +14,16 @@ import TodoList from './components/TodoList'
 import TodoFooter from './components/TodoFooter'
 
 export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    addTodo() {
+
+    }
+  },
   components: {
     'TodoHeader': TodoHeader,
     'TodoInput': TodoInput,
--- toDoList/src/components/TodoList.vue
+++ toDoList/src/components/TodoList.vue
@@ -14,6 +14,7 @@
 
 <script>
 export default {
+  props: ['propsdata'],
   data() {
     return {
       todoItems: []

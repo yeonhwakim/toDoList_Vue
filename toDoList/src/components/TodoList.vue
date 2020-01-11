b74b8a40ee4abcfa95a8fbd14c332cede4914a20
yeonhwa <yeonhwa6717@gmail.com>
2020-01-11 23:19:34 +0900

    Get localStorage list and set list, at first. - created 라이프 사이클 추가 - v-for로 loop - v-for 에러: v-bind:key="todoItem"추가

--- toDoList/src/components/TodoList.vue
+++ toDoList/src/components/TodoList.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">{{todoItem}}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created() {
+    if(localStorage.length >0) {
+      for(let i = 0; i < localStorage.length; i++){
+        this.todoItems.push(localStorage.key(i))
+      }
+    }
+  }
 }
 </script>
 
